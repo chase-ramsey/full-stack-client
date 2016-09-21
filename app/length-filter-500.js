d867f46766a7e20be083853056f7c306332c34e9
Chase Ramsey <ramsey.chase@gmail.com>
2016-09-20 21:00:22 -0500

    Review detail layout done

--- app/length-filter-500.js
+++ app/length-filter-500.js
@@ -1,11 +1,15 @@
 app.filter('lengthFilter500', () => {
   return (fullText) => {
-    if (fullText.length > 500) {
-      sliced_string = fullText.slice(0, 500);
-      new_text = sliced_string + '...';
-      return new_text;
+    if (!fullText) {
+      return;
     } else {
-      return fullText;
+        if (fullText.length > 500) {
+          sliced_string = fullText.slice(0, 500);
+          new_text = sliced_string + '...';
+          return new_text;
+        } else {
+          return fullText;
+        }
     }
   }
 })
---
+++ app/review/review-ctrl.js
@@ -0,0 +1,38 @@
+app.controller('ReviewCtrl', ['$scope', '$routeParams', '$http', '$timeout', 'RootFactory', function($scope, $routeParams, $http, $timeout, RootFactory) {
+
+  $scope.title = 'Review Detail';
+
+  $scope.showFullText = true;
+  $scope.showReviewStats = false;
+
+  let logError = (err) => console.log("error", err);
+
+  RootFactory.getApiRoot()
+    .then(
+      root => {
+        $scope.apiRoot = root;
+        return $http.get(`${$scope.apiRoot.reviews}${$routeParams.review_id}`);
+      },
+      logError
+    )
+    .then(
+      reviewRes => {
+        $scope.thisReview = reviewRes.data;
+        console.log("thisReview: ", $scope.thisReview);
+        $scope.watsonReport = JSON.parse($scope.thisReview.watson_report);
+        console.log("watson report: ", $scope.watsonReport);
+        $timeout();
+      }
+    )
+
+  $scope.toggleShow = (show) => {
+    if (show === 'text') {
+      $scope.showReviewStats = false;
+      $scope.showFullText = true;
+    } else if (show === 'stats') {
+      $scope.showFullText = false;
+      $scope.showReviewStats = true;
+    }
+  }
+
+}]);
