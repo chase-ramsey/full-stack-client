96243ca4de21838262451e109eeb85f274cd94db
Chase Ramsey <ramsey.chase@gmail.com>
2016-09-22 05:39:52 -0500

    If not logged in, manage/create reroute to login

--- app/auth-factory.js
+++ app/auth-factory.js
@@ -1,6 +1,20 @@
 app.factory('AuthFactory', () => {
 
+  let userCreds = {};
+
   return {
+
+    checkCreds: (creds) => {
+      if (creds) {
+          userCreds = creds;
+        } else {
+          if (userCreds.hasOwnProperty("password")) {
+            return window.btoa(`${userCreds.username}:${userCreds.password}`);
+          } else {
+            return false;
+          }
+        }
+    }
 
   }
 
--- app/route-config.js
+++ app/route-config.js
@@ -1,35 +1,49 @@
+
+let requiresAuth = ($location, AuthFactory) => new Promise((resolve, reject) => {
+  if (AuthFactory.checkCreds()) {
+    console.log("User is authenticated, resolve route promise");
+    resolve();
+  } else {
+    console.log("User is not authenticated, reject route promise");
+    reject();
+    $location.path("/login");
+  }
+});
+
 app.config(($routeProvider) => {
-    $routeProvider
-      .when('/', {
-        controller: 'MainCtrl',
-        templateUrl: 'app/main/main.html'
-      })
-      .when('/users/:user_id', {
-        controller: 'UserCtrl',
-        templateUrl: 'app/user/user.html'
-      })
-      .when('/reviews/:review_id', {
-        controller: 'ReviewCtrl',
-        templateUrl: 'app/review/review-detail.html'
-      })
-      .when('/lists/:list_id', {
-        controller: 'ListCtrl',
-        templateUrl: 'app/list/list-detail.html'
-      })
-      .when('/login', {
-        controller: 'LoginCtrl',
-        templateUrl: 'app/auth/login.html'
-      })
-      .when('/register', {
-        controller: 'RegisterCtrl',
-        templateUrl: 'app/auth/login.html'
-      })
-      .when('/create/review', {
-        controller: 'NewReviewCtrl',
-        templateUrl: 'app/create/new-review.html'
-      })
-      .when('/curate', {
-        controller: 'ManageListCtrl',
-        templateUrl: 'app/create/manage-lists.html'
-      })
+  $routeProvider
+    .when('/', {
+      controller: 'MainCtrl',
+      templateUrl: 'app/main/main.html'
+    })
+    .when('/users/:user_id', {
+      controller: 'UserCtrl',
+      templateUrl: 'app/user/user.html'
+    })
+    .when('/reviews/:review_id', {
+      controller: 'ReviewCtrl',
+      templateUrl: 'app/review/review-detail.html'
+    })
+    .when('/lists/:list_id', {
+      controller: 'ListCtrl',
+      templateUrl: 'app/list/list-detail.html'
+    })
+    .when('/login', {
+      controller: 'LoginCtrl',
+      templateUrl: 'app/auth/login.html'
+    })
+    .when('/register', {
+      controller: 'RegisterCtrl',
+      templateUrl: 'app/auth/login.html'
+    })
+    .when('/create/review', {
+      controller: 'NewReviewCtrl',
+      templateUrl: 'app/create/new-review.html',
+      resolve: { requiresAuth }
+    })
+    .when('/curate', {
+      controller: 'ManageListCtrl',
+      templateUrl: 'app/create/manage-lists.html',
+      resolve: { requiresAuth }
+    })
   });
